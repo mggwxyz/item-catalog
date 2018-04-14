bcd898ebcbf56c3cbccbebf67d5ea286653aa820
Michael Gilbertson <mggwxyz@gmail.com>
2018-04-14 16:53:22 -0400

    Added all new files

--- catalog/application.py
+++ catalog/application.py
@@ -1,13 +1,59 @@
 #!/usr/bin/python3
+from sqlalchemy import create_engine, Integer, String, Column, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from flask import Flask, render_template, url_for
+from database import Base, Item, Category
 
-from flask import Flask
-app = Flask(__name__)
+APP = Flask(__name__, template_folder='./templates')
+
+engine = create_engine('sqlite:///item_catalog.db', convert_unicode=True)
+Base.metadata.bind = engine
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-@app.route("/")
+@APP.route("/")
 def hello():
     return "Hello World!"
 
+
+@APP.route("/categories")
+def view_all_categories():
+    all_categories = session.query(Category)
+    return render_template('categories.html', categories=all_categories)
+
+
+@APP.route("/categories/<int:category_id>/")
+def view_category(category_id):
+    results = session.query(Category).filter(Category.id == category_id)
+    category = results.first()
+    print(category)
+    template = '<p>category: {}, description: {}</p><br>'
+    output = template.format(category.name, category.description)
+    return output
+
+
+@APP.route("/categories/new/")
+def create_new_category():
+    output = 'Return create new category page...'
+    print(output)
+    return output
+
+
+@APP.route("/categories/<int:category_id>/edit/")
+def edit_category():
+    output = 'Return edit category page...'
+    print(output)
+    return output
+
+
+@APP.route("/categories/<int:category_id>/delete/")
+def delete_category():
+    output = 'Return delete category page...'
+    print(output)
+    return output
+
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    APP.debug = True
+    APP.run(host='0.0.0.0', port=8000)
---
+++ catalog/database.py
@@ -0,0 +1,58 @@
+from sqlalchemy import create_engine, Integer, String, Column, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+# ORM classes
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=True)
+
+
+class Item(Base):
+
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+# Create engine and session required for querying
+engine = create_engine('sqlite:///item_catalog.db', convert_unicode=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+# Insert initial values into category table
+def insert_categories():
+    print('Inserting categories...')
+    session.add_all([
+        Category(name='Hygiene', description='Items related to hygiene'),
+        Category(name='Technology', description='Items related to technology'),
+        Category(name='Food', description='Items related to food'),
+        Category(name='Clothing', description='Items that are worn'),
+        Category(name='Books', description='Items that are books')
+    ])
+    session.commit()
+
+
+# Insert initial values into items table
+def insert_items():
+    print('Inserting items...')
+
+
+def init_db():
+    insert_categories()
+    insert_items()
+
+if __name__ == '__main__':
+    init_db()
